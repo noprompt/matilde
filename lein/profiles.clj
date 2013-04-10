046d962ad6c54305293d26442e2e0454ced2a63c
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-04-09 22:18:55 -0700

    Lein: Add kibit and swank to plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins  [[lein-ring "0.8.2"]
                    [lein-cljsbuild "0.3.0"]
-                   [lein-clojars "0.9.1"]]}}
+                   [lein-clojars "0.9.1"]
+                   [lein-swank "1.4.5"]
+                   [lein-kibit "0.0.8"]]}}
