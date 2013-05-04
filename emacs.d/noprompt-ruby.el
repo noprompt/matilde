2cad7f83b3e80c929bc5a2f5c12c665b73edc2e8
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-05-03 17:57:07 -0700

    Update ruby

--- emacs.d/noprompt-ruby.el
+++ emacs.d/noprompt-ruby.el
@@ -2,8 +2,6 @@
 (require 'ruby-mode)
 (require 'ruby-end)
 
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (define-key evil-visual-state-local-map "s" 'ruby-send-region)))
+(setq ruby-end-insert-newline nil)
 
 (provide 'noprompt-ruby)
