b38951134cf42de52907058c913f26b91df4d1cc
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-05-08 12:10:29 -0700

    Lein: Update profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,8 @@
                    [lein-cljsbuild "0.3.0"]
                    [lein-clojars "0.9.1"]
                    [lein-swank "1.4.5"]
-                   [lein-kibit "0.0.8"]]}}
+                   [lein-kibit "0.0.8"]]
+        :injections [(require 'spyscope.core)
+                     (require '[redl core complete])]
+        :dependencies [[redl "0.1.0"]
+                       [spyscope "0.1.2"]]}}
