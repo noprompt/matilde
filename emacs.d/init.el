dcee0d0ed4b0987520c45631be552844ba19c477
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-05-17 22:27:00 -0700

    Emacs: The eighties

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,7 +18,7 @@
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 
 ;; Color theme
-;;(load-theme 'sanityinc-tomorrow-night t)
+(load-theme 'sanityinc-tomorrow-night-eighties t)
 
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
@@ -227,7 +227,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "95113fe659fed51f34f230cc3bfb29271595fc67a5ed76c5ec33c85e7d8e3685" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "c5c8c61c9ea795c410d7ddfee26b5ff8156383235af333cc51563164b0d14390" default))))
+ '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "95113fe659fed51f34f230cc3bfb29271595fc67a5ed76c5ec33c85e7d8e3685" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "c5c8c61c9ea795c410d7ddfee26b5ff8156383235af333cc51563164b0d14390" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
