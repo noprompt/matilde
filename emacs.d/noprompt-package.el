d793cdb6ff39b7529ba99e86ae8627ed5db24c1a
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-08-27 17:43:45 -0700

    Emacs: Add noctilux theme

--- emacs.d/noprompt-package.el
+++ emacs.d/noprompt-package.el
@@ -2,6 +2,8 @@
 
 (add-to-list 'package-archives
             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+            '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
