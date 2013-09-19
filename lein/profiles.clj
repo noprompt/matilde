058d1cd5f9a21193abe4197c9530eab7e8f3bb59
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-19 02:41:22 -0700

    Lein: Add clojure.tools.namespace

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,12 +3,9 @@
                    [lein-clojars "0.9.1"]
                    [lein-swank "1.4.5"]
                    [lein-kibit "0.0.8"]]
-        :injections [(require 'spyscope.core)
-                     (require '[redl core complete])]
-        :dependencies [[redl "0.1.0"]
-                       [spyscope "0.1.2"]
-                       [javert "0.2.0-SNAPSHOT"]
-                       [ritz/ritz-nrepl-middleware "0.7.0"]]
+        :dependencies [[javert "0.2.0-SNAPSHOT"]
+                       [ritz/ritz-nrepl-middleware "0.7.0"]
+                       [org.clojure/tools.namespace "0.2.4"]]
         :repl-options {:nrepl-middleware
                        [inspector.middleware/wrap-inspect
                         ritz.nrepl.middleware.javadoc/wrap-javadoc
