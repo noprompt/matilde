5a05d63f795d2bf6f75634d1d5e419d6e826ee4d
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-12-13 11:00:56 -0800

    Lein: Update profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,14 @@
-{:user {:plugins  [[lein-ring "0.8.2"]
-                   [lein-cljsbuild "0.3.0"]
-                   [lein-clojars "0.9.1"]
-                   [lein-swank "1.4.5"]
+{:user {:plugins  [[lein-clojars "0.9.1"]
                    [lein-kibit "0.0.8"]]
         :dependencies [[javert "0.2.0-SNAPSHOT"]
+                       [clj-stacktrace "0.2.7"]
                        [ritz/ritz-nrepl-middleware "0.7.0"]
                        [org.clojure/tools.namespace "0.2.4"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly (deref new))))]
         :repl-options {:nrepl-middleware
                        [inspector.middleware/wrap-inspect
                         ritz.nrepl.middleware.javadoc/wrap-javadoc
