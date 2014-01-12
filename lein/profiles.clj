b387f9d3396c89a6856732c10f76eaefd910cd1c
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-01-12 12:54:23 -0800

    Lein: Add lein-environ

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins  [[lein-clojars "0.9.1"]
-                   [lein-kibit "0.0.8"]]
+                   [lein-kibit "0.0.8"]
+                   [lein-environ "0.4.0"]]
         :dependencies [[javert "0.2.0-SNAPSHOT"]
                        [clj-stacktrace "0.2.7"]
                        [ritz/ritz-nrepl-middleware "0.7.0"]
