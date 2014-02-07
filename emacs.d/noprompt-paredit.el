45abdf22f293b10e7e177957955276071c234b07
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-02-06 16:27:35 -0800

    Emacs: "namespace" custom paredit commands Add two new sexp transpose functions for paredit

--- emacs.d/noprompt-paredit.el
+++ emacs.d/noprompt-paredit.el
@@ -1,9 +1,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Paredit
+
 (package-require 'paredit)
 (require 'paredit)
 
-(defun paredit-wrap-quote ()
+(defun noprompt/paredit-wrap-quote ()
   "Wrap the following sexp in double quotes."
   (interactive)
   (save-excursion
@@ -11,20 +12,39 @@
     (forward-sexp)
     (insert "\"")))
 
+(defun noprompt/forward-transpose-sexps ()
+  (interactive)
+  (paredit-forward)
+  (transpose-sexps 1)
+  (paredit-backward))
+
+(defun noprompt/backward-transpose-sexps ()
+  (interactive)
+  (transpose-sexps 1)
+  (paredit-backward)
+  (paredit-backward))
+
 (defun noprompt/define-paredit-keys ()
   (progn
-    (nlmap "W("  'paredit-wrap-round)
-    (nlmap "W["  'paredit-wrap-square)
-    (nlmap "W{"  'paredit-wrap-curly)
-    (nlmap "W\"" 'paredit-wrap-quote)
-    (nlmap "("   'paredit-backward-slurp-sexp)
-    (nlmap ")"   'paredit-backward-barf-sexp)
-    (nlmap "{"   'paredit-forward-barf-sexp)
-    (nlmap "}"   'paredit-forward-slurp-sexp)
-    (nlmap "S"   'paredit-splice-sexp)
-    (nlmap "s"   'paredit-split-sexp)
-    (nlmap "T"   'transpose-sexps)
-    (nlmap "t"   'clojure-test-run-test)
-    (nlmap "D"   'paredit-kill)))
+    (nlmap "W("          'paredit-wrap-round)
+    (nlmap "W["          'paredit-wrap-square)
+    (nlmap "W{"          'paredit-wrap-curly)
+    (nlmap "W\""         'noprompt/paredit-wrap-quote)
+    (nlmap "("           'paredit-backward-slurp-sexp)
+    (nlmap ")"           'paredit-backward-barf-sexp)
+    (ilmap (kbd "C-(")   'paredit-backward-slurp-sexp)
+    (ilmap (kbd "C-)")   'paredit-backward-barf-sexp)
+    (nlmap "{"           'paredit-forward-barf-sexp)
+    (nlmap "}"           'paredit-forward-slurp-sexp)
+    ;; I don't like the inconsistency here but C-{ and C-} don't seem
+    ;; to work.
+    (ilmap (kbd "C-[")   'paredit-forward-barf-sexp)
+    (ilmap (kbd "C-]")   'paredit-forward-slurp-sexp)
+    (nlmap "S"           'paredit-splice-sexp)
+    (nlmap "s"           'paredit-split-sexp)
+    (nlmap "T"           'noprompt/backward-transpose-sexps)
+    (nlmap "t"           'noprompt/forward-transpose-sexps)
+    (nlmap "Y"           'paredit-copy-as-kill)
+    (nlmap "D"           'paredit-kill)))
 
 (provide 'noprompt-paredit)
